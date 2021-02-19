7ac556c54e81925c08b797e63751ea0383b1a797
JustinInthaly21 <825134@mystma.org>
2021-02-19 19:14:23 +0000

    last10 4

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def last10(a, b, c, d, e):
-    if e == 10:
+def last10(a, b, c, d, e, f):
+    if f == 10:
         return True
     else:
         return False
 
-print(last10(0, 0, 0, 0, 0))
+print(last10(0, 0, 0, 0, 0, 0))
